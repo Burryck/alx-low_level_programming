c4a9837462070a5fb7084568abdfbf5f2dc21594
Burryck <festusamuzu@gmail.com>
2023-03-16 23:12:40 +0000

    Printf statement

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This is a printf checker
+*/
+int main(void)
+/* This is a printf */
+{
+	printf("With proper grammar, but the outcome is a piece of art", \n)
+		return (0);
+}
