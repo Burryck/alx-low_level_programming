04a9b4634dd65303904a0061720afb90738b30c0
Burryck <festusamuzu@gmail.com>
2023-03-18 03:36:53 +0000

    Printing positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+
+/**
+  * main - Determines if a number is positive, negative or zero.
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	int n;
@@ -16,12 +20,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-	       printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
 }
