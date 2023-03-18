3aa498c5d4e685715e799c1d94d9f6e7f7c5f3fa
Burryck <festusamuzu@gmail.com>
2023-03-18 01:13:47 +0000

    Printing lower and uppercase alp

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print alphabet in lowercae and in uppercase
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
