b1f75fbabf92e5b5ed25350ddaca06401837ec9e
Burryck <festusamuzu@gmail.com>
2023-03-18 01:48:26 +0000

    Single digits base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Main - Description: Printing single numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
