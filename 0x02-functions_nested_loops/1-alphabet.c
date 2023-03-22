aa1d83a85db4706dcecb5a924afc031383f0cdc9
Burryck <festusamuzu@gmail.com>
2023-03-22 14:40:39 +0000

    Files from nestted loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  * Description: 'Prints alphabets in lowercase'
+  *
+ */
+void print_alphabet(void)
+{
+	int a;
+	for(a = 97; a <=122; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  * Description: 'Prints alphabets in lowercase'
+  *
+ */
+void print_alphabet(void)
+{
+	int a,b;
+	for(a = 97; a <=122; a++)
+	{
+		for(b = 0; b <= 10, b++);
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
