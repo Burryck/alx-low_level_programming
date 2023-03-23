43944bc75177b2b3a8d70aaa56024f90e5357e3f
Burryck <festusamuzu@gmail.com>
2023-03-23 19:30:10 +0000

    More functios

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int _isuppe(int c)
+{
+	char c;
+		for (c = 41; c < 59; c++)
+		{
+			putchar();
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is new nestted loops 0x04. C - More functions, more nested loops
