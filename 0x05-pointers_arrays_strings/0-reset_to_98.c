0ea3057c71a132df5c8bb70326f3eeff3a4ab116
Burryck <festusamuzu@gmail.com>
2023-03-28 01:10:26 +0000

    Resetting a num to a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *main - resetting a point
+  *@reset_to_98 - reset n to 98
+  *Return: 0.
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - check the code
+  * Description: This is done by change of subject
+  * @: a is a pointer for swapping *b
+  * @: b is a pointer for swapping *a
+  * @swap_int: is used to swap the integers
+  * Return: Always 0.
+  */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = c;
+	*c = *a;
+}
