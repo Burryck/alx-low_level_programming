e6692b6758ccc0f22fabec1cd3659f96e044d6a7
Burryck <festusamuzu@gmail.com>
2023-04-01 04:40:36 +0000

    8-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,6 +10,7 @@ char *rot13(char *s)
 	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 	int i;
 	int j = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; part1[j] != '\0'; j++)
